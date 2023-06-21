d9c59a52e91046a4f45cb9e7a9763902599fb5de
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-06-21 10:26:23 +0530

    Added: Using Spring Concept employee Payroll Application bulid

---
+++ src/main/java/com/bridgeLabz/employeepayrollapp/controller/EmployeePayrollController.java
@@ -0,0 +1,35 @@
+package com.bridgeLabz.employeepayrollapp.controller;
+
+import com.bridgeLabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employeepayrollservice")
+public class EmployeePayrollController {
+    @RequestMapping(value = {"","/","/get"})
+    public ResponseEntity<String> getEmployeePayrollData(){
+        return new ResponseEntity<String>("Get Call Success", HttpStatus.OK);
+    }
+    @GetMapping("/get/{empId}")
+    public ResponseEntity<String> getEmployeePayrollData(@PathVariable("empId") int empId){
+        return new ResponseEntity<String>("Get Call Success for id:" +empId, HttpStatus.OK);
+    }
+    @PostMapping("/create")
+    public ResponseEntity<String> addEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO)
+    {
+        return  new ResponseEntity<String>("Created Employee Payroll Data for:" +employeePayrollDTO,HttpStatus.OK);
+    }
+    @PutMapping("/update")
+    public ResponseEntity<String> updateEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO)
+    {
+        return  new ResponseEntity<String>("Updated Employee Payroll Data for:" +employeePayrollDTO,HttpStatus.OK);
+    }
+    @DeleteMapping("/delete/{empId}")
+    public ResponseEntity<String> deleteEmployeePayrollData(@PathVariable("empId") int empId){
+        return new ResponseEntity<String>("Delete Call Success for id:" +empId, HttpStatus.OK);
+    }
+
+}
